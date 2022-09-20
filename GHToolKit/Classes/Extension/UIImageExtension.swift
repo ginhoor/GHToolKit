671accadf802e11567c70add390ef6e4e3605d8c
Ginhoor <ginhoor@gmail.com>
2022-09-20 22:12:58 +0800

    feat: 更新ICON

--- GHToolKit/Classes/Extension/UIImageExtension.swift
+++ GHToolKit/Classes/Extension/UIImageExtension.swift
@@ -10,21 +10,21 @@ import Foundation
 public struct SFSymbolsIcon { }
 
 public extension SFSymbolsIcon {
-    static let log = UIImage(imageLiteralResourceName: "LogIcon")
+    static let vehicle = UIImage(systemName: "car")
+    static let add = UIImage(systemName: "plus.circle.fill")
+
     static let history = UIImage(systemName: "memories")
     static let journeys = UIImage(systemName: "globe")
-    static let vehicle = UIImage(systemName: "car")
     static let vehicleiOS13 = UIImage(systemName: "car.fill")
     static let settings = UIImage(systemName: "gear")
     static let share = UIImage(systemName: "square.and.arrow.up")
     static let moreActions = UIImage(systemName: "ellipsis.circle")
     static let delete = UIImage(systemName: "trash")
-    static let add = UIImage(systemName: "plus.circle.fill")
     static let edit = UIImage(systemName: "square.and.pencil")
-    static let importFiles = UIImage(systemName: "square.and.arrow.down.on.square.fill")
+    static let importFiles = UIImage(systemName: "square.and.arrow.down.on.square")
+    static let importFilesFill = UIImage(systemName: "square.and.arrow.down.on.square.fill")
     static let photo = UIImage(systemName: "photo")
     static let split = UIImage(systemName: "square.split.2x1")
-    static let device = UIImage(imageLiteralResourceName: "deviceIcon")
     static let filter = UIImage(systemName: "line.horizontal.3.decrease.circle")
     static let tag = UIImage(systemName: "tag")
     static let calendar = UIImage(systemName: "calendar")
