2261d8d12cf2b8bd6ea09f97370106d87078f2f7
Ginhoor <ginhoor@gmail.com>
2023-06-09 00:01:15 +0800

    feat: 增加swiftui 扩展

--- GHToolKit/Classes/Core/Tool/GHSwiftUIContext.swift
+++ GHToolKit/Classes/Core/Tool/GHSwiftUIContext.swift
@@ -40,3 +40,20 @@ public extension GHSwiftUIContext {
         return true
     }
 }
+
+public extension View {
+    @ViewBuilder func ifIs<T>(_ condition: Bool, transform: (Self) -> T) -> some View where T: View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+    @ViewBuilder func ifElse<T:View,V:View>( _ condition:Bool,isTransform:(Self) -> T,elseTransform:(Self) -> V) -> some View {
+        if condition {
+            isTransform(self)
+        } else {
+            elseTransform(self)
+        }
+    }
+}
